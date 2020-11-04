2e05893f849482f1ed2d62d4702fa8e6cd541bab
IsMondayTMR <47375192+IsMondayTMR@users.noreply.github.com>
2020-11-04 15:10:44 -0800

    for merge

--- app/src/App.js
+++ app/src/App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import './App.css';
 import Header from "./containers/header"
 import Routers from "./Routers/Routers"
-import UserStore from './stores/UserStore';
 import {UserInfor} from './context/userInfo'
+
 function App() {
+
   return (
     <div className="App">
       <UserInfor>
