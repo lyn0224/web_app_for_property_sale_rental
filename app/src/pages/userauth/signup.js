525d1d217b7838ab9882b2a9f9c596f35258c0b7
Reneechen108 <reneechen108@gmail.com>
2020-11-05 16:11:01 -0800

    Add realtor signup form

--- app/src/pages/userauth/signup.js
+++ app/src/pages/userauth/signup.js
@@ -8,21 +8,19 @@ function Signup() {
 
     const history = useHistory();
     // const { firebase } = useContext(FirebaseContext);
-    const [firstName, setFirstName] = useState('');
+    const [username, setUsername] = useState('');
     const [emailAddress, setEmailAddress] = useState('');
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
-    
-    const isInvalid = firstName === '' || password === '' || emailAddress === '';
+    const [realtor, setRealtor] = useState(false);
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [zipcode, setZipCode] = useState('');
+    const [phone, setPhone] = useState('');
     
     async function handleSignup (event){
+        //console.log(username, emailAddress, password, firstName, lastName, zipcode, phone);
         event.preventDefault();
-            if(!emailAddress){
-                return;
-            }
-            if(!password){
-                return;
-            }
             try{
                 let res = await fetch('http://localhost:9000/register', {
                     method: 'post',
@@ -31,9 +29,13 @@ function Signup() {
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify({
-                        username: firstName,
+                        username: username,
                         emailAddress : emailAddress,
                         password: password,
+                        firstName: firstName,
+                        lastName: lastName,
+                        zipcode: zipcode,
+                        phone: phone
                     })
                 });
                 let result = await res.json();
@@ -52,43 +54,82 @@ function Signup() {
             }
     }
     
+    function handleInputChange(event) {
+        const target = event.target;
+        const value = target.type === 'checkbox' ? target.checked : target.value;
+        setRealtor(!realtor);
+    }
+    const isInvalid = !realtor ? username === '' || password === '' || emailAddress === '' :
+    firstName === '' || lastName === '' || zipcode === '' || phone === '' || username === '' || password === '' || emailAddress === '';
+
+    const conditionalRealtor = realtor ? 
+    <Form.Base>
+        <Form.Input
+            placeholder="First Name"
+            value={firstName}
+            onChange={({ target }) => setFirstName(target.value)}
+        />
+        <Form.Input
+            placeholder="Last Name"
+            value={lastName}
+            onChange={({ target }) => setLastName(target.value)}
+        />
+        <Form.Input
+            placeholder="Zip Code"
+            value={zipcode}
+            onChange={({ target }) => setZipCode(target.value)}
+        />
+        <Form.Input
+            placeholder="Phone Number"
+            value={phone}
+            onChange={({ target }) => setPhone(target.value)}
+        />
+    </Form.Base> : null;
     return (
         <>
            <Form>
-                    <Form.Title>Sign Up</Form.Title>
-                    {error && <Form.Error>{error}</Form.Error>}
-                    
-                    <Form.Base onSubmit={handleSignup} method="POST">
-                        <Form.Input
-                            placeholder="First Name"
-                            value={firstName}
-                            onChange={({ target }) => setFirstName(target.value)}
-                        />
-                        <Form.Input
-                            placeholder="Email Address"
-                            value={emailAddress}
-                            onChange={({ target }) => setEmailAddress(target.value)}
-                        />
-                        <Form.Input
-                            type="password"
-                            value={password}
-                            autoComplete="off"
-                            placeholder="Password"
-                            onChange={({ target }) => setPassword(target.value)}
-                        />
-                        <Form.Submit disabled={isInvalid} type="submit">
-                            Sign Up
-                        </Form.Submit>
-                        
-                        <Form.Text>
-                            Already a user? <Form.Link to={ROUTES.SIGN_IN}>Sign In now.</Form.Link>
-                        </Form.Text>
-                        <Form.TextSmall>
-                            This page is protected by Google reCAPTCHA.
-                        </Form.TextSmall>
-                    </Form.Base>
-                </Form>
-                <Footer/>
+                <Form.Title>Sign Up</Form.Title>
+                {error && <Form.Error>{error}</Form.Error>}
+                
+                <Form.Base onSubmit={handleSignup} method="POST">
+                    <Form.Input
+                        placeholder="User Name"
+                        value={username}
+                        onChange={({ target }) => setUsername(target.value)}
+                    />
+                    <Form.Input
+                        placeholder="Email Address"
+                        value={emailAddress}
+                        onChange={({ target }) => setEmailAddress(target.value)}
+                    />
+                    <Form.Input
+                        type="password"
+                        value={password}
+                        autoComplete="off"
+                        placeholder="Password"
+                        onChange={({ target }) => setPassword(target.value)}
+                    />
+                    <label>
+                        <input
+                            name="realtor"
+                            type="checkbox"
+                            checked={realtor}
+                            onChange={handleInputChange} />
+                            I am landlord or industry professional
+                    </label>
+                    {conditionalRealtor}
+                    <Form.Submit disabled={isInvalid} type="submit">
+                        Sign Up
+                    </Form.Submit>
+                    <Form.Text>
+                        Already a user? <Form.Link to={ROUTES.SIGN_IN}>Sign In now.</Form.Link>
+                    </Form.Text>
+                    <Form.TextSmall>
+                        This page is protected by Google reCAPTCHA.
+                    </Form.TextSmall>
+                </Form.Base>
+            </Form>
+            <Footer/>
         </>
     )
 }
