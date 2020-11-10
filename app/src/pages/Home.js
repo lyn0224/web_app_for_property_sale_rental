a1092d272fa41c7620aff8175ea57b0a6e6c2a96
Reneechen108 <reneechen108@gmail.com>
2020-11-10 00:32:35 -0800

    search bar

--- app/src/pages/Home.js
+++ app/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Footer from "../containers/footer"
 import Gridcard from "../containers/gridcard"
- 
+import Search from '../containers/search'
 
 function Home(){
     return(
